56b9d13b25d7e6e557f43330b7efa94a3f0d2ea0
umanzoor02 <UsamaManzoor0260@gmail.com>
2023-08-13 00:02:03 +0500

    ToDo App Final

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,8 +3,12 @@ from todos.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by('-updated_at')
+
+    completed_tasks = Task.objects.filter(is_completed = True)
     # because i want to display the task on the front end
     context = {
-     'tasks':tasks
+     'tasks':tasks,
+     'completed_tasks':completed_tasks,
+
     }
     return render(request, 'home.html', context)
--- todos/urls.py
+++ todos/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    path('marks_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done' ),
+    path('marks_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone' ),
+    path('edit/<int:pk>/', views.edit, name='edit'), 
+    path('delete/<int:pk>/', views.delete, name='delete'), 
 ]
